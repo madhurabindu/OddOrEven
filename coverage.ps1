68d6dae09c11cf0e6dfcc820e12d716a5bfcecb8
mb <madhura.bindu@gmail.com>
2020-09-04 15:52:35 +0530

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -1,7 +1,23 @@
-[xml]$doc = Get-Content -Path UnitTestProject\TestResults\*\coverage.cobertura.xml | Out-String
+param($linerate)
+
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+$report = Get-Content -Path UnitTestProject\TestResults\*\coverage.cobertura.xml | Out-String
 Write-Host "---------------------------------"
 Write-Host "Code Coverage report ..." 
 Write-Host "---------------------------------"
+WriteXmlToScreen $report
+
+[xml]$doc = $report
 
 Write-Host ""
 Write-Host "---------------------------------"
