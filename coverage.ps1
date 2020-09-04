805286f11e818e934357bb1353df90b11567a4d5
mb <madhura.bindu@gmail.com>
2020-09-04 13:07:58 +0530

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -35,9 +35,9 @@ Write-Host ""
     }
 
 if($result -eq 1){
-    Write-Host "Coverage Check: Passed" -ForegroundColor green 
+    Write-Host "Coverage Check: failed" -ForegroundColor red 
 }
 else{
-    Write-Host "Coverage Check: failed" -ForegroundColor red 
+    Write-Host "Coverage Check: Passed" -ForegroundColor green 
 }
 exit $result
