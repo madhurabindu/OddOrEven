36bfbe047f866df63a55bb0935fc5f92338e29c3
Madhura Bindu <madhura.bindu@xymob.in>
2020-09-04 12:08:43 +0530

    ps files

--- coverage.ps1
+++ coverage.ps1
@@ -1,18 +1,44 @@
+param($linerate)
 
-Write-Host "Start" 
+function WriteXmlToScreen ([xml]$xml)
+{
+    $StringWriter = New-Object System.IO.StringWriter;
+    $XmlWriter = New-Object System.Xml.XmlTextWriter $StringWriter;
+    $XmlWriter.Formatting = "indented";
+    $xml.WriteTo($XmlWriter);
+    $XmlWriter.Flush();
+    $StringWriter.Flush();
+    Write-Output $StringWriter.ToString();
+}
+
+Write-Host "---------------------------------"
+Write-Host "Code Coverage report ..." 
+Write-Host "---------------------------------"
 [xml]$doc = Get-Content -Path UnitTestProject\TestResults\*\coverage.cobertura.xml
 
+WriteXmlToScreen $doc
+
+Write-Host ""
+Write-Host "---------------------------------"
+Write-Host "Code Coverage report Analysis..." 
+Write-Host "---------------------------------"
 
 $result = 1
 
-Write-Host "Line Coverage: ["$doc.coverage.'line-rate'"] Branch Coverage: ["$doc.coverage.'branch-rate'"]"
+Write-Host "Line-Coverage: ["$doc.coverage.'line-rate'"] Branch-Coverage: ["$doc.coverage.'branch-rate'"]"
 Write-Host ""
  foreach ($pkg in $doc.coverage.packages.package) {
-    Write-Host "Package:"  $pkg.name "Line Coverage:["$pkg.'line-rate'"]"
+    Write-Host "Package:"  $pkg.name "Line-Coverage:"$pkg.'line-rate'
 
-    if($pkg.'line-rate' -ne 1){
+    if($pkg.'line-rate' -le $linerate){
         $result= 0
        }
     }
-Write-Host "ExitCode:" $result
- exit $result
+
+if($result -eq 1){
+    Write-Host "Coverage Check: Passed" -ForegroundColor green 
+}
+else{
+    Write-Host "Coverage Check: failed" -ForegroundColor red 
+}
+exit $result
